af2c8ee7cbaccc3cbc00cb99260ccf5b34466d81
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-11-14 23:44:04 +0900

    Add slugify utility function and improve cn function formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function slugify(text?: string) {
+  if (!text) {
+    return Math.random().toString(36).substring(5);
+  }
+  return text
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[^\w-]+/g, "");
 }

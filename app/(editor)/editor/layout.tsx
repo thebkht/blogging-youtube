58f5ce1532d634370b73c07ba7c7b7a4a34f006a
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-11-15 01:41:32 +0900

    Add image upload functionality, popover component, and text formatting buttons to editor

--- app/(editor)/editor/layout.tsx
+++ app/(editor)/editor/layout.tsx
@@ -6,6 +6,6 @@ export default function EditorLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="mx-auto flex w-full max-w-7xl flex-col">{children}</div>
+    <div className="mx-auto flex w-full max-w-7xl flex-col p-4">{children}</div>
   );
 }
--- components/forms/content-field.tsx
+++ components/forms/content-field.tsx
@@ -7,105 +7,36 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { EditorContent, EditorRoot } from "novel";
 import {
-  TiptapImage,
-  TiptapLink,
-  UpdatedImage,
-  TaskList,
-  TaskItem,
-  HorizontalRule,
-  StarterKit,
-  Placeholder,
-} from "novel/extensions";
+  EditorBubble,
+  EditorCommand,
+  EditorCommandEmpty,
+  EditorCommandItem,
+  EditorCommandList,
+  EditorContent,
+  EditorRoot,
+} from "novel";
 import { generateJSON } from "@tiptap/html";
-import { cn } from "@/lib/utils";
 import { postFormSchema } from "@/lib/types";
-
-const placeholder = Placeholder;
-const tiptapLink = TiptapLink.configure({
-  HTMLAttributes: {
-    class: cn(
-      "text-muted-foreground underline underline-offset-[3px] hover:text-primary transition-colors cursor-pointer",
-    ),
-  },
-});
-
-const taskList = TaskList.configure({
-  HTMLAttributes: {
-    class: cn("not-prose pl-2"),
-  },
-});
-const taskItem = TaskItem.configure({
-  HTMLAttributes: {
-    class: cn("flex items-start my-4"),
-  },
-  nested: true,
-});
-
-const horizontalRule = HorizontalRule.configure({
-  HTMLAttributes: {
-    class: cn("mt-4 mb-6 border-t border-muted-foreground"),
-  },
-});
-
-const starterKit = StarterKit.configure({
-  bulletList: {
-    HTMLAttributes: {
-      class: cn("list-disc list-outside leading-3 -mt-2"),
-    },
-  },
-  orderedList: {
-    HTMLAttributes: {
-      class: cn("list-decimal list-outside leading-3 -mt-2"),
-    },
-  },
-  listItem: {
-    HTMLAttributes: {
-      class: cn("leading-normal -mb-2"),
-    },
-  },
-  blockquote: {
-    HTMLAttributes: {
-      class: cn("border-l-4 border-primary"),
-    },
-  },
-  codeBlock: {
-    HTMLAttributes: {
-      class: cn("rounded-sm bg-muted border p-5 font-mono font-medium"),
-    },
-  },
-  code: {
-    HTMLAttributes: {
-      class: cn("rounded-md bg-muted  px-1.5 py-1 font-mono font-medium"),
-      spellcheck: "false",
-    },
-  },
-  horizontalRule: false,
-  dropcursor: {
-    color: "#DBEAFE",
-    width: 4,
-  },
-  gapcursor: false,
-});
-
-export const defaultExtensions = [
-  starterKit,
-  placeholder,
-  TiptapLink,
-  TiptapImage,
-  UpdatedImage,
-  taskList,
-  taskItem,
-  horizontalRule,
-  tiptapLink,
-];
+import { slashCommand, suggestionItems } from "./editor/slash-command";
+import { Separator } from "../ui/separator";
+import { NodeSelector } from "./editor/selectors/node-selector";
+import { LinkSelector } from "./editor/selectors/link-selector";
+import { TextButtons } from "./editor/selectors/text-buttons";
+import { ColorSelector } from "./editor/selectors/color-selector";
+import { useState } from "react";
+import { defaultExtensions } from "./editor/extensions";
+import { handleCommandNavigation } from "novel/extensions";
 
 export function ContentField({
   form,
 }: {
   form: UseFormReturn<z.infer<typeof postFormSchema>>;
 }) {
+  const extensions = [...defaultExtensions, slashCommand];
+  const [openNode, setOpenNode] = useState(false);
+  const [openColor, setOpenColor] = useState(false);
+  const [openLink, setOpenLink] = useState(false);
   return (
     <>
       <FormField
@@ -122,9 +53,66 @@ export function ContentField({
                     const html = editor.getHTML();
                     form.setValue("content", html);
                   }}
-                  extensions={defaultExtensions}
-                  className="rounded-xl border p-4"
-                />
+                  extensions={extensions}
+                  editorProps={{
+                    handleDOMEvents: {
+                      keydown: (_view, event) => handleCommandNavigation(event),
+                    },
+                    // handlePaste: (view, event) =>
+                    //   handleImagePaste(view, event, uploadFn),
+                    // handleDrop: (view, event, _slice, moved) =>
+                    //   handleImageDrop(view, event, moved, uploadFn),
+                    attributes: {
+                      class: `prose prose-lg dark:prose-invert prose-headings:font-title font-default focus:outline-none max-w-full`,
+                    },
+                  }}
+                  className="rounded-xl border"
+                >
+                  <EditorCommand className="z-50 h-auto max-h-[330px] overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all">
+                    <EditorCommandEmpty className="px-2 text-muted-foreground">
+                      No results
+                    </EditorCommandEmpty>
+                    <EditorCommandList>
+                      {suggestionItems.map((item) => (
+                        <EditorCommandItem
+                          value={item.title}
+                          onCommand={(val) => item.command?.(val)}
+                          className={`flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:bg-accent aria-selected:bg-accent`}
+                          key={item.title}
+                        >
+                          <div className="flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background">
+                            {item.icon}
+                          </div>
+                          <div>
+                            <p className="font-medium">{item.title}</p>
+                            <p className="text-xs text-muted-foreground">
+                              {item.description}
+                            </p>
+                          </div>
+                        </EditorCommandItem>
+                      ))}
+                    </EditorCommandList>
+                  </EditorCommand>
+                  <EditorBubble
+                    tippyOptions={{
+                      placement: "bottom-start",
+                    }}
+                    className="flex w-fit max-w-[90vw] overflow-hidden rounded-md border border-muted bg-background shadow-xl"
+                  >
+                    <Separator orientation="vertical" />
+                    <NodeSelector open={openNode} onOpenChange={setOpenNode} />
+                    <Separator orientation="vertical" />
+
+                    <LinkSelector open={openLink} onOpenChange={setOpenLink} />
+                    <Separator orientation="vertical" />
+                    <TextButtons />
+                    <Separator orientation="vertical" />
+                    <ColorSelector
+                      open={openColor}
+                      onOpenChange={setOpenColor}
+                    />
+                  </EditorBubble>
+                </EditorContent>
               </EditorRoot>
             </FormControl>
             <FormMessage />

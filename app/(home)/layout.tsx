5590a69405301c2f5d88ce4d1990635951b6260e
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-12-10 03:54:38 +0900

    Refactor layout structure and integrate Tabs component for enhanced navigation

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,9 +2,9 @@ import SiteSidebar from "@/components/site-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <SiteSidebar />
+    <div className="mx-auto flex max-w-4xl">
+      <SiteSidebar className="left-auto" />
       <div className="flex-1 overflow-y-auto p-4">{children}</div>
-    </>
+    </div>
   );
 }
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,18 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function HomePage() {
-  return <>Home Page</>;
+  return (
+    <>
+      <Tabs defaultValue="foryou" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="foryou">For You</TabsTrigger>
+          <TabsTrigger value="following">Following</TabsTrigger>
+        </TabsList>
+        <TabsContent value="foryou">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="following">Change your password here.</TabsContent>
+      </Tabs>
+    </>
+  );
 }
